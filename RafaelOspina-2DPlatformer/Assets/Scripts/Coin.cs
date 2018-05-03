8a3422f2d50be161baebcf6002d83db756bac731
Rafael Ospina <rafaelospiname@gmail.com>
2018-05-02 21:58:49 -0500

    Monedas

---
+++ RafaelOspina-2DPlatformer/Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public AudioClip clip;
+	public float volume;
+	AudioSource sonido;
+	bool alreadyPlayed = false;
+
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent <AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D () {
+		if (alreadyPlayed == false) {
+			sonido.PlayOneShot (clip, volume);
+			alreadyPlayed = true;
+		}
+	}
+}
